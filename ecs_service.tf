d6c969118353f4f2206755ad4dd66f2033640f19
Arabasta <keiyamx@gmail.com>
2026-02-21 14:53:02 +0800

    feat: add user service

--- ecs_service.tf
+++ ecs_service.tf
@@ -41,3 +41,25 @@ resource "aws_ecs_service" "preference" {
 
   depends_on = [aws_lb_listener.http]
 }
+
+resource "aws_ecs_service" "user" {
+  name            = "${var.name}-user"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.user.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = data.aws_subnets.default.ids
+    security_groups  = [aws_security_group.ecs_tasks.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.user.arn
+    container_name   = "user"
+    container_port   = 8080
+  }
+
+  depends_on = [aws_lb_listener.http]
+}
--- ecs_task.tf
+++ ecs_task.tf
@@ -57,3 +57,33 @@ resource "aws_ecs_task_definition" "preference" {
     }
   ])
 }
+
+resource "aws_ecs_task_definition" "user" {
+  family                   = "${var.name}-user"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+
+  execution_role_arn = aws_iam_role.ecs_task_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name  = "user"
+      image = var.user_image
+      portMappings = [{
+        containerPort = 8080
+        protocol      = "tcp"
+      }]
+      essential = true
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          awslogs-group         = aws_cloudwatch_log_group.ecs.name
+          awslogs-region        = var.aws_region
+          awslogs-stream-prefix = "ecs"
+        }
+      }
+    }
+  ])
+}
