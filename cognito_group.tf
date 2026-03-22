8550739e75a87d6f09b74c9a02c6f7da46a79f6d
Arabasta <keiyamx@gmail.com>
2026-03-22 08:12:24 +0800

    fix: missed file

--- cognito_group.tf
+++ cognito_group.tf
@@ -13,8 +13,8 @@ resource "aws_cognito_user_group" "app_user" {
 }
 
 # web client
-resource "aws_cognito_user_pool_client" "admin" {
-  name         = "${var.name}-admin-client-${var.env}"
+resource "aws_cognito_user_pool_client" "web" {
+  name         = "${var.name}-web-client-${var.env}"
   user_pool_id = aws_cognito_user_pool.this.id
 
   generate_secret = true
@@ -59,8 +59,29 @@ resource "aws_cognito_user_pool_client" "mobile" {
   ]
 }
 
-output "cognito_admin_client_id" {
-  value = aws_cognito_user_pool_client.admin.id
+resource "aws_cognito_user_pool_client" "test" {
+  name         = "${var.name}-test-client-${var.env}"
+  user_pool_id = aws_cognito_user_pool.this.id
+
+  generate_secret                      = false
+  allowed_oauth_flows_user_pool_client = true
+  allowed_oauth_flows                  = ["code"]
+  allowed_oauth_scopes                 = ["email", "openid", "profile"]
+
+  callback_urls = var.cognito_callback_urls
+  logout_urls   = var.cognito_logout_urls
+
+  supported_identity_providers = ["COGNITO"]
+
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH"
+  ]
+}
+
+output "cognito_web_client_id" {
+  value = aws_cognito_user_pool_client.web.id
 }
 
 output "cognito_mobile_client_id" {
@@ -75,6 +96,10 @@ output "cognito_app_user_group_name" {
   value = aws_cognito_user_group.app_user.name
 }
 
-output "cognito_admin_login_url" {
-  value = "https://${aws_cognito_user_pool_domain.this.domain}.auth.${data.aws_region.current.id}.amazoncognito.com/login?client_id=${aws_cognito_user_pool_client.admin.id}&response_type=code&scope=openid+email+profile&redirect_uri=${urlencode(var.cognito_callback_urls[0])}"
+output "cognito_web_login_url" {
+  value = "https://${aws_cognito_user_pool_domain.this.domain}.auth.${data.aws_region.current.id}.amazoncognito.com/login?client_id=${aws_cognito_user_pool_client.web.id}&response_type=code&scope=openid+email+profile&redirect_uri=${urlencode(var.cognito_callback_urls[0])}"
+}
+
+output "cognito_test_login_url" {
+  value = "https://${aws_cognito_user_pool_domain.this.domain}.auth.${data.aws_region.current.id}.amazoncognito.com/login?client_id=${aws_cognito_user_pool_client.test.id}&response_type=code&scope=openid+email+profile&redirect_uri=${urlencode("https://dev.keiyam.me/oauth2/idpresponse")}"
 }
--- cognito_pool.tf
+++ cognito_pool.tf
@@ -1,5 +1,6 @@
 resource "aws_cognito_user_pool" "this" {
   name = "${var.name}-user-pool-${var.env}"
+  username_attributes      = ["email"]
 
   password_policy {
     minimum_length    = 8
@@ -11,12 +12,6 @@ resource "aws_cognito_user_pool" "this" {
 
   auto_verified_attributes = ["email"]
 
-  schema {
-    attribute_data_type = "String"
-    name                = "email"
-    required            = true
-    mutable             = false
-  }
 
   schema {
     attribute_data_type = "String"
@@ -36,29 +31,9 @@ resource "aws_cognito_user_pool" "this" {
   }
 }
 
-resource "aws_cognito_user_pool_client" "this" {
-  name         = "${var.name}-client-${var.env}"
-  user_pool_id = aws_cognito_user_pool.this.id
-
-  generate_secret                      = true
-  allowed_oauth_flows_user_pool_client = true
-  allowed_oauth_flows                  = ["code", "implicit"]
-  allowed_oauth_scopes                 = ["email", "openid", "profile"]
-  callback_urls                        = var.cognito_callback_urls
-  logout_urls                          = var.cognito_logout_urls
-
-
-  explicit_auth_flows = [
-    "ALLOW_USER_PASSWORD_AUTH",
-    "ALLOW_REFRESH_TOKEN_AUTH",
-    "ALLOW_USER_SRP_AUTH"
-  ]
-}
-
 resource "aws_cognito_user_pool_domain" "this" {
   domain                = "${var.cognito_domain_prefix}-${var.env}"
   user_pool_id          = aws_cognito_user_pool.this.id
-  managed_login_version = 2
 }
 
 output "cognito_user_pool_id" {
