5872498bd75b2f1f1eba6b5847d14d54831a7082
gh-joshua <limguanhua00@gmail.com>
2026-04-10 16:18:14 +0800

    Auto-commit: alb_listeners.tf

--- alb_listeners.tf
+++ alb_listeners.tf
@@ -90,3 +90,33 @@ resource "aws_lb_listener_rule" "user" {
     }
   }
 }
+
+resource "aws_lb_listener_rule" "llm" {
+  listener_arn = aws_lb_listener.https.arn
+  priority     = 30
+
+  action {
+    type = "authenticate-cognito"
+
+    authenticate_cognito {
+      user_pool_arn       = aws_cognito_user_pool.this.arn
+      user_pool_client_id = aws_cognito_user_pool_client.web.id
+      user_pool_domain    = aws_cognito_user_pool_domain.this.domain
+
+      session_cookie_name        = "AWSELBAuthSessionCookie"
+      session_timeout            = 36000
+      on_unauthenticated_request = "allow"
+    }
+  }
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.llm.arn
+  }
+
+  condition {
+    path_pattern {
+      values = ["/llm*"]
+    }
+  }
+}
